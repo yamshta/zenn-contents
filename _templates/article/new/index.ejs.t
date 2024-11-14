eedb09cd7d04412d761477a257ceb3f7c9cedbe1
toono <52028799+toono-f@users.noreply.github.com>
2024-11-14 12:58:45 +0900

    READMEとスクリプトのポート番号を3060に変更し、新しい記事テンプレートにバリデーションを追加

--- _templates/article/new/index.ejs.t
+++ _templates/article/new/index.ejs.t
@@ -3,14 +3,26 @@ to: articles/<%= h.inflection.dasherize(slug) %>.md
 ---
 
 ---
-title: "記事のタイトル" # 記事のタイトル
-emoji: "🎉" # 記事のアイコン
-type: "tech" # tech: 技術記事 / idea: アイデア
-topics: [] # タグ: ["next.js", "rails", "aws"] など
-publication_name: "<%= publication_name ? 'lclco' : '' %>" # 公開するpublication名（紐づけない場合は空文字）
+title: "<%= title %>" # 記事のタイトル
+emoji: "<%= emoji %>" # 記事のアイコン
+type: "<%= type %>" # 記事のタイプ tech: 技術記事 / idea: アイデア記事
+topics: [] # 記事のトピック ["next.js", "rails", "aws"] など（必須）
+publication_name: "<%= publication_name ? 'your_publication_name' : '' %>" # Publicationに紐付ける場合は公開するpublication名
 published: false # 公開するかどうか
 ---
 
 ## はじめに
 
-ここから本文を書きます。
+<!-- 記事の導入部分を書きましょう -->
+
+## 本文
+
+<!-- 記事の本文をここから書きましょう -->
+
+## まとめ
+
+<!-- 記事のまとめを書きましょう -->
+
+## 参考文献
+
+<!-- 参考にした記事やドキュメントがあれば記載しましょう -->
--- _templates/article/new/index.js
+++ _templates/article/new/index.js
@@ -1,21 +1,50 @@
-const inputValidator = (input) => {
-  if (input !== "") {
-    return true;
-  }
-};
-
 module.exports = [
   {
     type: "input",
     name: "slug",
     message: "記事のスラッグを入力してください（URLとして使用されます）",
-    validate: inputValidator,
-    initial: new Date().toISOString().split("T")[0].replace(/-/g, ""), // 空欄の場合は、本日日付をタイトルとする
+    validate: (input) => {
+      const pattern = /^[a-z0-9_-]{12,50}$/;
+      if (!input) return "スラッグは必須です";
+      if (!pattern.test(input)) {
+        return "スラッグは小文字の半角英数字(a-z0-9)、ハイフン(-)、アンダースコア(_)の12〜50字の組み合わせである必要があります";
+      }
+      return true;
+    },
+    initial: `article-${new Date().toISOString().split("T")[0]}`,
+  },
+  {
+    type: "input",
+    name: "emoji",
+    message: "記事のアイコンを入力してください（1文字の絵文字）",
+    validate: (input) => {
+      if (!input) return "絵文字は必須です";
+      if (input.length !== 2) return "1つの絵文字を入力してください";
+      return true;
+    },
+    initial: "✍️",
+  },
+  {
+    type: "select",
+    name: "type",
+    message: "記事のタイプを選択してください",
+    choices: ["tech", "idea"],
+    initial: "tech",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "記事のタイトルを入力してください",
+    validate: (input) => {
+      if (!input) return "タイトルは必須です";
+      return true;
+    },
+    initial: "記事のタイトルは未定です",
   },
   {
     type: "toggle",
     name: "publication_name",
-    message: "LCL EngineersのPublicationに紐付けますか？（後で変更可能です）",
+    message: "Publicationに紐付けますか？",
     initial: true,
   },
 ];
